59a6f7813b0e9ecad0211d2ee60794486568b9aa
BBun-bun <132358383+BBun-bun@users.noreply.github.com>
2023-07-01 16:48:23 +0100

    Concatenation Assignment

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -1,3 +1,5 @@
 var A = "This is a string";
 
-document.write("Liza told bart, \"Knock it off, bart of ill tell dad\"<br>\"eat my short!\"bart responded.");
+document.write("\"the cat is vicious,"
++ "the cat is crazy\" "
++ "the cat is insane");
