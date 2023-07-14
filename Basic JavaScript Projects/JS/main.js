72620c607965b1adfeec8ab51e7e5b9f6bf08fc1
BBun-bun <1anthony.mds@gmail.com>
2023-07-14 15:34:22 +0100

    added comments

--- Basic JavaScript Projects/JS/main.js
+++
@@ -1,3 +0,0 @@
-var A = "This is a string", B = "This is another string", C = "This is a third string", D = "This is a fourth string";
-
-document.write(6 + 6);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,3 @@
+var A = "This is a string", B = "This is another string", C = "This is a third string", D = "This is a fourth string"; //a list of variables
+
+document.write(6 + 6); //printing out an expression
