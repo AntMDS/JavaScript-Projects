81dcb8762e003d674e509fd5d12eddb507e6564c
BBun-bun <1anthony.mds@gmail.com>
2023-07-14 15:39:12 +0100

    Basic JavaScript Projects Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,5 @@
-var A = "This is a string", B = "This is another string", C = "This is a third string", D = "This is a fourth string"; //a list of variables
+var Sent1 = "This is a string", Sent2 = "This is another string"; //a list of variables
 
 document.write(6 + 6); //printing out an expression
+
+window.alert(Sent1 + Sent2)//makes a window alert concatenate the two variables
