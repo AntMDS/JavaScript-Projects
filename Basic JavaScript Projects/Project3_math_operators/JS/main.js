a875828ae947b7ec6174fb3fb46e81f40f37c308
BBun-bun <1anthony.mds@gmail.com>
2023-07-19 18:00:59 +0100

    Added more Math operators

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,24 @@
 function Add_Function() {
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML= "2 + 2 =" + addition;
+    document.getElementById("add").innerHTML= "2 + 2 =" + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("subtract").innerHTML = "5 - 2 =" + Subtraction;
+}
+
+function multiplication_Function() {
+    var multiplication = 6 * 8;
+    document.getElementById("multiply").innerHTML = "6 x 8 =" + multiplication;
+}
+
+function divisio_function() {
+    var division = 48 / 6;
+    document.getElementById("divide").innerHTML = "48 / 8 =" + division
+}
+
+function multiple_Math_function() {
+    var Multiple_Operators = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Multiple_Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 = "+ Multiple_Operators
 }
