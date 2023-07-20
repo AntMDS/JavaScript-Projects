effd3af4c9ea02abc3d5757aabaa3a952c4ae661
BBun-bun <1anthony.mds@gmail.com>
2023-07-20 14:04:06 +0100

    Project3_Math_Operators

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -22,3 +22,29 @@ function multiple_Math_function() {
     var Multiple_Operators = (1 + 2) * 10 / 2 -5;
     document.getElementById("Multiple_Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 = "+ Multiple_Operators
 }
+
+function modulus_operator() {
+    var modulus = 25 % 6;
+    document.getElementById("modulus_math").innerHTML = "when you divide 25 by 6 you have a remainder of: " + modulus
+}
+
+function negation_operator() {
+    var negation_x = 10
+    document.getElementById("negation_math").innerHTML = -negation_x
+}
+
+function increment_operator() {
+    var increment_x =5;
+    increment_x++;
+    document.write(increment_x)
+}
+
+function decrement_operator() {
+    var decrement_x = 5.25;
+    decrement_x--;
+    document.write(decrement_x);
+}
+
+function random_math() {
+    window.alert(Math.random() * 100);
+}
