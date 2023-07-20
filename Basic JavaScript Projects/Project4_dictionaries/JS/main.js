919bd0b741c6428e0c23f08facbefc87ecf79243
BBun-bun <1anthony.mds@gmail.com>
2023-07-20 14:21:26 +0100

    Project4_dictionaries

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function Dictionary() {
+    var Animals = {
+        Species:"Turtle",
+        Color:"Green",
+        Breed:"Sea turtle",
+        Age:"80",
+        Sound:"Hiss"
+    };
+    document.getElementById("dictionary").innerHTML = Animals.Sound
+}
